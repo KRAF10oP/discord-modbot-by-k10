bbdaa8846affd9b0a7e3e2844ada131270caefd7
KRAF10oP <88928954+KRAF10oP@users.noreply.github.com>
2023-09-24 13:58:35 +0530

    Add files via upload

---
+++ variables/variables.js
@@ -0,0 +1,9 @@
+module.exports = bot => {
+      bot.variables({
+            color: "00ffbe",
+            timeouttime: "10m", // you can change time out time from here :)
+            ownerrole: "OWNER ROLE ID HERE",
+            tm: "NONE", // 💀DONT TUCHE THIS PART DANGER 💀
+            reason: "NONE" // 💀DONT TUCHE THIS PART DANGER 💀
+      });
+    }
